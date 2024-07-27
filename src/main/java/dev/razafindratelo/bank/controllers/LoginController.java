e75766c288816199aa43532a5d3f642c7c67be44
Abega1642 <hei.abega.2@gmail.com>
2024-07-27 18:45:49 +0300

    feat: add Login controller

--- src/main/java/dev/razafindratelo/bank/controllers/LoginController.java
+++ src/main/java/dev/razafindratelo/bank/controllers/LoginController.java
@@ -1,4 +1,15 @@
 package dev.razafindratelo.bank.controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    public ChoiceBox acc_selector;
+    public Label payee_identifier;
+    public TextField payee_identifier_input;
+    public TextField password_input;
+    public Button login_btn;
+    public Label invalid_input_message;
 }
