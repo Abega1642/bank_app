76cbaa890e2d9eb2bffeb8a3e198315fe2bb6e76
Abega1642 <hei.abega.2@gmail.com>
2024-07-27 16:18:09 +0300

    feat: configure module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,10 @@ module dev.razafindratelo.bank {
     requires javafx.fxml;
     requires javafx.web;
 
+    requires de.jensd.fx.glyphs.fontawesome;
+    requires java.sql;
+    requires org.postgresql.jdbc;
+
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
@@ -10,4 +14,9 @@ module dev.razafindratelo.bank {
 
     opens dev.razafindratelo.bank to javafx.fxml;
     exports dev.razafindratelo.bank;
+    exports dev.razafindratelo.bank.controllers;
+    exports dev.razafindratelo.bank.controllers.admin;
+    exports dev.razafindratelo.bank.controllers.client;
+    exports dev.razafindratelo.bank.models;
+    exports dev.razafindratelo.bank.views;
 }
